bb8f6189f1d4a5fa7831bdf514b861b91b036755
Rafael Kallis <rafael@rafaelkallis.com>
2015-07-03 02:18:25 +0300

    arrays.cpp updated
    New class ArrayList_Ext
    Moved RemoveLast() and Collapse() from ArrayList to ArrayList_Ext
    ArrayList_Ext functions added Find() and ShrinkToFit()

--- arrays.cpp
+++ arrays.cpp
@@ -15,14 +15,9 @@ template <class T> void ArrayList<T>::Insert(T *Data){
     this->Data[size++]=*Data;
 }
 
-template <class T> void ArrayList<T>::RemoveLast(){
-    if(--size < capacity*0.45)Collapse();
-}
-
 template <class T> void ArrayList<T>::Set(int index, T *Data){
     if(index<0 || index>=this->size) throw OutofBoundsException();
     this->Data[index]=*Data;
-    delete Data;
 }
 
 template <class T> T* ArrayList<T>::Get(int index){
@@ -40,10 +35,6 @@ template<class T> int ArrayList<T>::Size(){
 }
 
 template <class T> void ArrayList<T>::Expand(){
-    /*T *temp=this->Data;
-    temp=(T*)realloc(temp,sizeof(T)*this->capacity*1.5);
-    if(!temp)throw ReallocException();
-    this->Data=temp,capacity*=1.5;*/
     T* Temp=new T[(int)(this->capacity*1.5)]();
     if(!Temp) throw ReallocException();
     capacity*=1.5;
@@ -52,28 +43,14 @@ template <class T> void ArrayList<T>::Expand(){
     this->Data=Temp;
 }
 
-template <class T> void ArrayList<T>::Collapse(){
-    /*T *temp=this->Data;
-    temp=(T*)realloc(temp,sizeof(T)*this->capacity/1.5);
-    if(!temp)throw ReallocException();
-    this->Data=temp,capacity/=1.5;*/
-    T* Temp=new T[(int)(this->capacity/1.5)]();
-    if(!Temp) throw ReallocException();
-    capacity/=1.5;
-    for (int i=0; i<this->size; i++) Temp[i]=this->Data[i];
-    delete [] this->Data;
-    this->Data=Temp;
-}
-
 template <class T> void ArrayList<T>::Init(){
     size=0,capacity=ARRAYLIST_INIT_CAP;
-    /*this->Data=(T*)malloc(sizeof(T)*ARRAYLIST_INIT_CAP); */
     this->Data=new T[ARRAYLIST_INIT_CAP]();
 }
 
 template <class T> void ArrayList<T>::Clean(){
-    /*free(Data);*/
     delete [] this->Data;
+    this->Data=NULL;this->size=this->capacity=0;
 }
 
 /*
@@ -81,18 +58,16 @@ template <class T> void ArrayList<T>::Clean(){
     An ArrayList with Extended functions
  */
 
-/*
 template <class T> void ArrayList_Ext<T>::RemoveLast(){
-    size--;
-    if(this->size < this->capacity*0.45)Collapse();
+    if(--this->size < this->capacity*0.45)Collapse();
 }
  
 template<class T> int ArrayList_Ext<T>::Find(T* Data){
-    for (int i=0; i<this->size; i++) if(Data==Get(i))return i;
+    for (int i=0; i<this->size; i++) if(this->Data==this->Get(i))return i;
     throw UncaughtException();
 }
 
-template<classT> void ArrayList_Ext<T>::ShrinkToFit(){
+template<class T> void ArrayList_Ext<T>::ShrinkToFit(){
     T* Temp=new T[(int)(this->size)]();
     if(!Temp) throw ReallocException();
     this->capacity=this->size;
@@ -104,9 +79,8 @@ template<classT> void ArrayList_Ext<T>::ShrinkToFit(){
 template <class T> void ArrayList_Ext<T>::Collapse(){
     T* Temp=new T[(int)(this->capacity/1.5)]();
     if(!Temp) throw ReallocException();
-    capacity/=1.5;
+    this->capacity/=1.5;
     for (int i=0; i<this->size; i++) Temp[i]=this->Data[i];
     delete [] this->Data;
     this->Data=Temp;
 }
- */

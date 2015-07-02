6109d19fa06aeccfced82f0fadfb1a717e179bf6
Rafael Kallis <rafael@rafaelkallis.com>
2015-07-03 01:34:09 +0300

    Replaced C Malloc() with C++ new
    Memory Leak fixed

--- arrays.cpp
+++ arrays.cpp
@@ -11,23 +11,23 @@
  */
 
 template <class T> void ArrayList<T>::Insert(T *Data){
-    if(this->size==this->capacity)Expand();
+    if(size==capacity)Expand();
     this->Data[size++]=*Data;
 }
 
 template <class T> void ArrayList<T>::RemoveLast(){
-    size--;
-    if(this->size < this->capacity*0.45)Collapse();
+    if(--size < capacity*0.45)Collapse();
 }
 
 template <class T> void ArrayList<T>::Set(int index, T *Data){
-    if(index>=this->size) throw OutofBoundsException();
-    else this->Data[index]=*Data;
+    if(index<0 || index>=this->size) throw OutofBoundsException();
+    this->Data[index]=*Data;
+    delete Data;
 }
 
 template <class T> T* ArrayList<T>::Get(int index){
     if(index<0 || index>=this->size) throw OutofBoundsException();
-    else return this->Data+index;
+    else return &this->Data[index];
 }
 
 template<class T> void ArrayList<T>::Reset(){
@@ -40,25 +40,73 @@ template<class T> int ArrayList<T>::Size(){
 }
 
 template <class T> void ArrayList<T>::Expand(){
-    T *temp=this->Data;
+    /*T *temp=this->Data;
     temp=(T*)realloc(temp,sizeof(T)*this->capacity*1.5);
     if(!temp)throw ReallocException();
-    this->Data=temp,capacity*=1.5;
+    this->Data=temp,capacity*=1.5;*/
+    T* Temp=new T[(int)(this->capacity*1.5)]();
+    if(!Temp) throw ReallocException();
+    capacity*=1.5;
+    for (int i=0; i<this->size; i++) Temp[i]=this->Data[i];
+    delete [] this->Data;
+    this->Data=Temp;
 }
 
 template <class T> void ArrayList<T>::Collapse(){
-    T *temp=this->Data;
+    /*T *temp=this->Data;
     temp=(T*)realloc(temp,sizeof(T)*this->capacity/1.5);
     if(!temp)throw ReallocException();
-    this->Data=temp,capacity/=1.5;
+    this->Data=temp,capacity/=1.5;*/
+    T* Temp=new T[(int)(this->capacity/1.5)]();
+    if(!Temp) throw ReallocException();
+    capacity/=1.5;
+    for (int i=0; i<this->size; i++) Temp[i]=this->Data[i];
+    delete [] this->Data;
+    this->Data=Temp;
 }
 
 template <class T> void ArrayList<T>::Init(){
     size=0,capacity=ARRAYLIST_INIT_CAP;
-    this->Data=(T*)malloc(sizeof(T)*ARRAYLIST_INIT_CAP);
+    /*this->Data=(T*)malloc(sizeof(T)*ARRAYLIST_INIT_CAP); */
+    this->Data=new T[ARRAYLIST_INIT_CAP]();
 }
 
 template <class T> void ArrayList<T>::Clean(){
-    while(--size>0)delete (this->Data+size);
-    free(this->Data);
+    /*free(Data);*/
+    delete [] this->Data;
 }
+
+/*
+    ArrayList_Ext
+    An ArrayList with Extended functions
+ */
+
+/*
+template <class T> void ArrayList_Ext<T>::RemoveLast(){
+    size--;
+    if(this->size < this->capacity*0.45)Collapse();
+}
+ 
+template<class T> int ArrayList_Ext<T>::Find(T* Data){
+    for (int i=0; i<this->size; i++) if(Data==Get(i))return i;
+    throw UncaughtException();
+}
+
+template<classT> void ArrayList_Ext<T>::ShrinkToFit(){
+    T* Temp=new T[(int)(this->size)]();
+    if(!Temp) throw ReallocException();
+    this->capacity=this->size;
+    for (int i=0; i<this->size; i++) Temp[i]=this->Data[i];
+    delete [] this->Data;
+    this->Data=Temp;
+}
+ 
+template <class T> void ArrayList_Ext<T>::Collapse(){
+    T* Temp=new T[(int)(this->capacity/1.5)]();
+    if(!Temp) throw ReallocException();
+    capacity/=1.5;
+    for (int i=0; i<this->size; i++) Temp[i]=this->Data[i];
+    delete [] this->Data;
+    this->Data=Temp;
+}
+ */
--- arrays.h
+++ arrays.h
@@ -16,22 +16,37 @@ template <class T> class ArrayList{
 public:
     ArrayList(){Init();}
     ~ArrayList(){Clean();}
-    void Insert(T *data);
-    void RemoveLast();
-    void Set(int index,T *data);
-    T* Get(int index);
-    void Reset();
-    int Size();
+    void    Insert(T *data);
+    void    RemoveLast();
+    void    Set(int index,T *data);
+    T*      Get(int index);
+    void    Reset();
+    int     Size();
     
 protected:
-    int size,capacity;
-    T* Data;
+    int     size,capacity;
+    T*      Data;
     
-    void Expand();
-    void Collapse();
-    void Init();
-    void Clean();
+    void    Expand();
+    void    Collapse();
+    void    Init();
+    void    Clean();
 };
+
+/*
+    ArrayList_Ext
+    An ArrayList with Extended functions
+ */
+
+/*template <class T> class ArrayList_Ext: public ArrayList<T>{
+public:
+    void RemoveLast();
+    int  Find(T* Data);
+    void ShrinkToFit();
+protected:
+    void Collapse();
+    
+};*/
 
 #include "arrays.cpp"
 #endif /* defined(__ADT__arrays__) */
